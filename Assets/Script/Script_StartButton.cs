9c571d0ad9d4a9cba004addd58162fa700f6c782
DimiPhantom <ljiho1998@naver.com>
2015-06-20 10:10:25 +0900

    1

---
+++ Assets/Script/Script_StartButton.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Script_StartButton : MonoBehaviour {
+
+	public Button StartButton;
+	public GameObject Ball;
+	// Use this for initialization
+	void Start () {
+		StartButton.onClick.AddListener (Clicked);
+	}
+	void Clicked()
+	{
+		Ball.GetComponent<Rigidbody2D> ().gravityScale = 10;
+		foreach (GameObject A in Script_DragButton.prefabs) {
+			A.GetComponent<Collider2D> ().isTrigger = false;
+			A.GetComponent<Rigidbody2D>().gravityScale=20;
+			A.GetComponent<Rigidbody2D>().angularDrag=0.6f;
+			A.GetComponent<Rigidbody2D>().drag=0.5f;
+			A.GetComponent<Script_DragToScreen>().enabled=false;
+		}
+	}
+}
